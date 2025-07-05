a5197cf034c3f48d7e00625cbc209184572473e7
Desislava Borisova <d3sity@gmail.com>
2025-07-05 13:09:11 +0200

    Initial project architecture setup

---
+++ BoardGameApp.Data/BoardGameAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoardGameApp.Data
+{
+    public class BoardGameAppDbContext : IdentityDbContext
+    {
+        public BoardGameAppDbContext(DbContextOptions<BoardGameAppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
