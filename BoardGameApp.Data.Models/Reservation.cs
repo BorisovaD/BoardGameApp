5b04cd67ebb9f1b80aebed776a1c870d952c6747
Desislava Borisova <d3sity@gmail.com>
2025-07-06 14:20:05 +0200

    Add Reservation and Ticket models

---
+++ BoardGameApp.Data.Models/Reservation.cs
@@ -0,0 +1,35 @@
+﻿namespace BoardGameApp.Data.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Reservation
+    {
+        [Key]
+        [Comment("Reservation Unique Identifier")]
+        public Guid Id { get; set; }
+
+        [Comment("Time when the reservation was made")]
+        public DateTime ReservationTime { get; set; }
+
+        [Required]
+        [Comment("Identifier of the user")]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual BoardgameUser User { get; set; } = null!;
+
+        [Required]
+        [Comment("Identifier of the game session")]
+        public Guid GameSessionId { get; set; }
+
+        [ForeignKey(nameof(GameSessionId))]
+        public virtual GameSession GameSession { get; set; } = null!;
+    }
+}
---
+++ BoardGameApp.Data.Models/Ticket.cs
@@ -0,0 +1,41 @@
+﻿namespace BoardGameApp.Data.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
+    public class Ticket
+    {
+        [Key]
+        [Comment("Unique identifier for the ticket")]
+        public Guid Id { get; set; }
+
+        [Comment("Date and time when the ticket was issued")]
+        public DateTime IssuedOn { get; set; }
+        
+        [Comment("Ticket price")]
+        public decimal Price { get; set; }
+
+        [Comment("Tickets quantity bought")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Comment("Identifier of the user")]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual BoardgameUser User { get; set; } = null!;
+
+        [Required]
+        [Comment("Identifier of the user")]
+        public Guid ReservationId { get; set; }
+
+        [ForeignKey(nameof(ReservationId))]
+        public virtual Reservation Reservation { get; set; } = null!;
+    }
+}
