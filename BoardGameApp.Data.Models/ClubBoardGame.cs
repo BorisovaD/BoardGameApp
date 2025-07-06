34646d783d5ee3c917aaa5ba8030da8f8f9f5c8f
Desislava Borisova <d3sity@gmail.com>
2025-07-06 11:26:50 +0200

    Add ClubBoardGame mapping

---
+++ BoardGameApp.Data.Models/ClubBoardGame.cs
@@ -0,0 +1,32 @@
+﻿namespace BoardGameApp.Data.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [PrimaryKey(nameof(ClubId), nameof(BoardGameId))]
+    public class ClubBoardGame
+    {        
+        [Required]
+        [Comment("Identifier of the game club")]
+        public Guid ClubId { get; set; }
+
+        [ForeignKey(nameof(ClubId))]
+        public virtual Club Club { get; set; } = null!;
+
+        [Required]
+        [Comment("Identifier of the game played in this club")]
+        public Guid BoardGameId { get; set; }
+
+        [ForeignKey(nameof(BoardGameId))]
+        public virtual BoardGame BoardGame { get; set; } = null!;
+
+        [Comment("Indicates if the game is actively offered in this club")]
+        public bool IsDeleted { get; set; }
+    }
+}
