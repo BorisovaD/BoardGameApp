e2e34c536bec744cf80452f069e8fae3256d6c03
Desislava Borisova <d3sity@gmail.com>
2025-07-28 20:27:09 +0200

    Add Admin Dashboard view with styled management panels

---
+++ BoardGameApp/Areas/Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,13 @@
+﻿namespace BoardGameApp.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserManagementController : BaseAdminController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
